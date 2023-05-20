a48610e7472acfe33ad0389878b4f47147107ffc
Daniel <panero.daniel@gmail.com>
2023-05-20 20:41:46 +0200

    increased debug functions

--- debug.asm
+++ debug.asm
@@ -1,25 +1,59 @@
 ; Debugging library using the UART interface
 
+
+; Write a message into the console
+; - @0 message
 .macro DBMSG
     PRINTF	UART0_putc
-    .db	CR, @0, " " CR, 0
+    .db	CR, @0, CR, 0
 
 .endmacro
 
+; Write a message and the register values into the console
+; - @0 message
+; - @1 register (the binary value will be output)
 .macro DBREG
     mov a0, @1
     clr a1
 
     PRINTF	UART0_putc
-    .db	CR, @0, " ", FBIN, a, CR, 0
+    .db	CR, @0, FBIN, a, CR, 0
 
 .endmacro
 
+; Write a message and the register values into the console with formatting
+; - @0 message
+; - @1 format (FBIN, FHEX, FDEC, FCHAR, FSTR)
+.macro DBREGF
+    mov a0, @2
+    clr a1
+
+    PRINTF	UART0_putc
+    .db	CR, @0, @1, a, CR, 0
+
+.endmacro
+
+; Write a message and the registers values into the console
+; - @0 message
+; - @1 register high
+; - @2 register low
 .macro DBREGS
     mov a1, @1
     mov a0, @2
 
     PRINTF	UART0_putc
-    .db	CR, @0, " ", FBIN, a, CR, 0
+    .db	CR, @0, FBIN, a, CR, 0
+
+.endmacro
+
+; Write a message and the register IO values into the console
+; - @0 message
+; - @1 IO register (the binary value will be output)
+.macro DBIO
+    in a0, @1
+    clr a1
+
+    PRINTF	UART0_putc
+    .db	CR, @0, FBIN, a, CR, 0
 
 .endmacro
