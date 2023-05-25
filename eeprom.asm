1cb599b39acc6ba83f49154b93b2a3f8955d8355
Daniel <panero.daniel@gmail.com>
2023-05-25 17:45:47 +0200

    added time waiting

--- eeprom.asm
+++ eeprom.asm
@@ -2,6 +2,8 @@
 .equ	EEPROM	= 0b10100000	; device address
 .equ	R	= 1					; read flag
 
+
+; TODO at first start check whether some flag is initialized and if not initialize default settings
 eeprom_init:
     OUTI DDRB,0xff
     OUTI PORTB,0xff
@@ -15,6 +17,7 @@ eeprom_init:
 
 ; TODO change order of parameters to standard reg, address and not address, reg
 .macro EEPROM_READ
+    WAIT_US 1000
     CA i2c_start,EEPROM
     CA i2c_write, high(@0)
 	CA i2c_write, low(@0)
@@ -26,19 +29,23 @@ eeprom_init:
 
     rcall i2c_no_ack
 	rcall i2c_stop
+    WAIT_US 1000
 
 .endmacro 
 
 .macro EEPROM_WRITE
+    WAIT_US 1000
     CA i2c_start, EEPROM
     CA i2c_write, high(@0)
 	CA i2c_write, low(@0)
 
     CA i2c_write, @1
     rcall i2c_stop
+    WAIT_US 1000
 .endmacro
 
 .macro EEPROM_WRITE_REG
+    WAIT_US 1000
     CA i2c_start,EEPROM
     CA i2c_write, high(@0)
 	CA i2c_write, low(@0)
@@ -46,4 +53,5 @@ eeprom_init:
     mov a0, @1
     rcall i2c_write
     rcall i2c_stop
+    WAIT_US 1000
 .endmacro
