e001834bf3ca4b5d73511399d8ffdd6a9e0b132f
Daniel <panero.daniel@gmail.com>
2023-05-28 15:33:15 +0200

    buffer

--- main.asm
+++ main.asm
@@ -6,6 +6,9 @@
     rjmp reset
 
 
+.org INT7addr
+    jmp cin_remote_service_routine
+
 .org ADCCaddr
 	jmp	analog_service_routine
 
@@ -20,49 +23,12 @@
 
 ; Global variables (+analog_flag=r20 --> to be removed)
 .def note_index = r21
-.def status_flag = r22
-.def _status_flag = r23 ; FIXME remove it using the stack
 
 .include "eeprom.asm"
 .include "sound.asm"
 .include "record.asm"
 .include "analog.asm"
-
-.equ MODE = 1
-.equ MODEMSK = 0b00000011
-
-.equ MODE_WAITING = 0b00
-.equ MODE_PLAY = 0b01
-
-.equ PLAY_MODE = 3
-.equ PLAY_MODEMSK = 0b00001100
-
-.equ PLAY_MODE_FREE = 0b00
-.equ PLAY_MODE_RECORD = 0b01
-.equ PLAY_MODE_FROM_RECORD = 0b11
-
-; Custom macros
-.macro WJP1
-; in port, bit, timeout, address if P=1
-	ldi	w,@2+1
-	dec	w
-	breq	PC+4
-	sbis	@0,@1
-	rjmp	PC-3	
-    rjmp    @3 ; Jumps if P=1
-.endmacro
-
-
-.macro SWJP1
-; in port, bit, timeout, address if P=1, address if timeout
-	ldi	w,@2+1
-	dec	w
-	brne	PC+2
-    rjmp    @4 ; Jumps if time out
-	sbis	@0,@1
-	rjmp	PC-4	
-    rjmp    @3 ; Jumps if P=1
-.endmacro
+.include "cin.asm"
 
 reset:
     LDSP RAMEND ; Load stack pointer SP
@@ -80,101 +46,31 @@ reset:
     rcall sound_init
     rcall record_init
     rcall analog_init
+    rcall cin_init
 
     OUTI DDRD, 0 ; Buttons
-
-    ldi status_flag, 0b00000000 ; Menu at start
+    OUTI DDRE, 0 ; FIXME change it
 
     rjmp main
 
-; TODO Replace bit constant with correct equ
-; TODO Status flag management
+
 main:
-    ldi note_index, 0
-    rcall record_push
-
-    ldi note_index, 1
-    rcall record_push
-
-    ldi note_index, 2
-    rcall record_push
-
-    ldi note_index, 3
-    rcall record_push
-
-    ldi note_index, 4
-    rcall record_push
-
-    ldi note_index, 5
-    rcall record_push
-
-    ldi note_index, 6
-    rcall record_push
-
-    ldi note_index, 7
-    rcall record_push
-
-    rcall record_save_EEPROM
-
-    DBMSG "Loading from EEPROM:"
-    WAIT_MS 500
-    rcall record_load_EEPROM
-
-loop:
-    rcall play_free
-    rjmp loop
-
-    ; This part will make sense when using the interrupt as the buttons don't have one, having a status flag is more or less useless
-    JP1 PIND, 0, PC+3
-    INVB status_flag, 0
-    JP1 PIND, 1,  PC+3
-    INVB status_flag, 2
-    JP1 PIND, 2, PC+3
-    INVB status_flag, 3
+    clr d0
+main_loop:
+    ;WAIT_MS 100
     
-    ;DBREG "Status flag 0:", status_flag
+    ;rcall LCD_clear
+    CLR3 d1,d2,d3
+    PRINTF LCD_putc
+    .db CR,CR, "Enter a d0=", FDEC, d, "    ", CR, 0
 
-    push _status_flag ; Preserving scratch register content
+    CIN_NUM d0, main_loop
 
-    ;1. Determining the mode
-    mov _status_flag, status_flag
-    andi _status_flag, MODEMSK
-    ;DBREG "Status flag 1:", _status_flag
-
-    cpi _status_flag, 0b00000000; Check if waiting
-    brne PC+4
-    pop _status_flag
-    rcall waiting
-    rjmp main
-
-    ;2. Determining the play mode
-    mov _status_flag, status_flag
-    andi _status_flag, PLAY_MODEMSK
-    ;DBREG "Status flag 2:", _status_flag
-
-    ;2.1. Check if play free mode
-    cpi _status_flag, 0b00000000
-    brne PC+4
-    pop _status_flag
-    rcall play_free
-    rjmp main
-
-    ;2.2. Check if play and record mode
-    cpi _status_flag, 0b00000100
-    brne PC+4
-    pop _status_flag
-    rcall play_and_record
-    rjmp main
-
-    ;2.3. Check if play from record mode
-    cpi _status_flag, 0b00001100
-    brne PC+4
-    pop _status_flag
-    rcall play_from_record
-    rjmp main
-
-    pop _status_flag
-    rjmp main
+    rcall LCD_clear
+    PRINTF LCD_putc
+    .db CR,CR, "d0=", FDEC, d, CR, 0
+end:
+    rjmp end
 
 ; TODO replace it with menu
 waiting:
@@ -190,7 +86,7 @@ play_free:
     ;.db CR, CR, "Playing free", "     ", CR, 0
 
     rcall analog_loop
-    ;DBREGF "Note index", FDEC, note_index
+    ;DBREGF "Note index: ", FDEC, note_index
     rcall sound_play_note
     ret
 
@@ -245,13 +141,9 @@ stop_record:
     .db CR, CR, "Do you want to listen to it?", CR, 0
     WAIT_MS 2500
 
-    WJP1 PIND, 0, 254, rewind_record_and_play
-
     PRINTF LCD_putc
     .db "Do you want to rewind the recording?", CR, 0
     WAIT_MS 2500
-
-    SWJP1 PIND, 0, 254, restart_record, rewind_record_and_record
 
 ; Restarts the recording from the same point
 restart_record:
